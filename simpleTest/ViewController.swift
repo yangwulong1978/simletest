72d5fd3e8a684732ad62ab3d007fd31c9df5c069
yangwulong1978 <yangwulong@MacBook-Pro.local>
2017-01-24 14:30:27 +1100

    Initial Commit

---
+++ simpleTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleTest
+//
+//  Created by yang wulong on 24/1/17.
+//  Copyright © 2017 q6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
