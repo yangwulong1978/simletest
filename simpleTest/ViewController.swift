99cba8c9b6d9d965f6808d6eaafc5df0aa2b3906
wulong yang <yangwulong0418@gmail.com>
2017-02-06 10:15:45 +1100

    teset

--- simpleTest/ViewController.swift
+++ simpleTest/ViewController.swift
@@ -13,13 +13,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+ 
 }
 
+extension Optional {
+   
+     func !!<T>(wrapped:T? ,failureText:@autoclosure () ->String) ->T {
+        if let x = wrapped{return x}
+        fatalError(failureText())
+    }
+}
+
+//extension Array where {
+//    static func ==<T:Equatable>(lhs: [T?], rhs: [T?])->Bool {
+//        return lhs.elementsEqual(rhs){$0 == $1}
+//}
+//}
+extension Sequence where Iterator.Element: Hashable {
+    
+    func unique() -> [Iterator.Element]{
+        
+        var seen: Set < Iterator.Element >= []
+        return filter {
+            
+            if seen.contains($0) {
+                return false
+            } else {
+                
+                seen.insert($0)
+                return true
+            }
+        }
+    }
+}
